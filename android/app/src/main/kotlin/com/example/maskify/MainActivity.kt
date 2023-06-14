e974229913d46c4a43269a36363317bc6d7d3e3d
Vlad Nechaev <nechaevvlad06@gmail.com>
2023-06-14 19:42:54 +0300

    Login bug fixes

--- android/app/src/main/kotlin/com/example/maskify/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/maskify/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.maskify
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
